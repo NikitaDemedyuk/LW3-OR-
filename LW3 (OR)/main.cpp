e2a94c88bb771871c835d5759d69750f8a0548bc
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-26 19:40:12 +0300

    Initial Commit

---
+++ LW3 (OR)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LW3 (OR)
+//
+//  Created by Никита on 3/26/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
